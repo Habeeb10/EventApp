7f6296ed80b7934bff3d24908ca89b95dda54bbc
Tijani Habeeb <79145763+habeeb10@users.noreply.github.com>
2021-07-09 10:28:49 +0100

    feat: created map card

---
+++ src/screens/event/utils/card.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Exit, Vector } from "../../../../assets/svg";
+import * as colors from "../../../common/colors";
+export const MapCard = ({ title, description, onDelete }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.vectorbox}>
+        <Vector />
+
+        <View style={styles.titlebox}>
+          <Text style={styles.title}>{title}</Text>
+          <Text>{description}</Text>
+        </View>
+      </View>
+
+      <View style={styles.svgbox}>
+        <TouchableOpacity onPress={onDelete}>
+          <Exit />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.editbox}>
+          <Text style={styles.edit}>EDIT</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  title: {
+    marginBottom: 10,
+  },
+  titlebox: {
+    marginLeft: 20,
+    alignItems: "flex-start",
+  },
+  svgbox: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    alignSelf: "center",
+  },
+  vectorbox: {
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+  },
+
+  edit: {
+    color: colors.deepblue,
+    fontSize: 12,
+  },
+  editbox: {
+    width: 60,
+    height: 30,
+    backgroundColor: "skyblue",
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    marginLeft: 10,
+  },
+
+  container: {
+    borderWidth: 1,
+    width: "95%",
+    height: 80,
+    backgroundColor: colors.wheat,
+    borderWidth: 1,
+    borderColor: "skyblue",
+    borderRadius: 8,
+    alignSelf: "center",
+    flexDirection: "row",
+    marginTop: 30,
+    paddingHorizontal: 15,
+    justifyContent: "space-between",
+  },
+});
