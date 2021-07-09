ab3682dc6ea2c92bd9018a9e62ecc30b48e05c91
Tijani Habeeb <79145763+habeeb10@users.noreply.github.com>
2021-07-09 10:28:33 +0100

    feat: reuseable input

---
+++ src/shared/Input.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { View, TextInput, StyleSheet, Text } from "react-native";
+import * as colors from "../common/colors";
+
+export const Input = ({
+  placeHolder,
+  style,
+  label,
+  inputStyle,
+  type,
+  onChange,
+  secure,
+  value,
+}) => {
+  return (
+    <View style={{ marginTop: 40 }}>
+      <View style={[styles.container, style]}>
+        <TextInput
+          style={[styles.input, inputStyle]}
+          placeholder={placeHolder}
+          placeholderTextColor={colors.deepgray}
+          value={value}
+          keyboardType={type}
+          onChangeText={onChange}
+          secureTextEntry={secure}
+        />
+      </View>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.wheat,
+    alignSelf: "center",
+    width: "90%",
+    borderRadius: 10,
+    height: 55,
+    marginTop: 15,
+    alignItems: "flex-start",
+    justifyContent: "center",
+    borderWidth: 1,
+    borderColor: "skyblue",
+  },
+  input: {
+    fontSize: 18,
+    color: colors.deepblue,
+    marginLeft: 15,
+    width: "100%",
+  },
+});
