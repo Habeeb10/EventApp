7368fbb17ca27688f7b20667f9d6767132b92ad4
Tijani Habeeb <79145763+habeeb10@users.noreply.github.com>
2021-07-09 10:29:02 +0100

    feat: app container

---
+++ src/component/container.js
@@ -0,0 +1,13 @@
+import { SafeAreaView, View } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+
+export const Container = ({ children, barColor, backgroundColor }) => {
+  return (
+    <>
+      <StatusBar style={"dark"} />
+      <SafeAreaView style={{ backgroundColor: barColor }} />
+      <View style={{ flex: 1, backgroundColor }}>{children}</View>
+    </>
+  );
+};
