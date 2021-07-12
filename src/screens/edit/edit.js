d3a80cb5170a853e89a187937fc7a313ef54f323
Tijani Habeeb <79145763+habeeb10@users.noreply.github.com>
2021-07-12 14:10:38 +0100

    refactor: edit screen

--- src/screens/edit/edit.js
+++ src/screens/edit/edit.js
@@ -4,38 +4,47 @@ import { Container } from "../../component/container";
 import { Editstyles as styles } from "./styles";
 import { Textinput } from "../../shared/textinput";
 import { BASE_URL } from "../../common/constant";
+import { Button } from "../../shared/button";
 
 export function Edit({ navigation, route }) {
   const { title, description, id } = route.params;
-  const [newTitle, setTitle] = useState(title);
-  const [newDescription, setDesription] = useState(description);
+  const [newTitle, setnewTitle] = useState(title);
+  const [newDescription, setnewDesription] = useState(description);
+  const [loading, setLoading] = useState(false);
 
   const handleEdit = () => {
     const data = {
       title: newTitle,
       description: newDescription,
+      startDate: new Date(),
+      endDate: new Date(),
+      createdAt: new Date(),
     };
-
+    setLoading(true);
     fetch(`${BASE_URL}/${id}`, { method: "PUT", data })
       .then((response) => response.json())
       .then((res) => {
         console.log(res);
+
         if (res.status === "success") {
           navigation.navigate("event");
-          return;
         }
       })
-      .catch((err) => err);
+      .catch((err) => err)
+      .finally(() => setLoading(false));
   };
 
   return (
     <>
       <Container>
-        <Textinput onChange={setTitle} value={newTitle} />
-        <Textinput onChange={setDesription} value={newDescription} />
-        <TouchableOpacity style={styles.edit} onPress={handleEdit}>
-          <Text>Submit Editting</Text>
-        </TouchableOpacity>
+        <Textinput onChange={setnewTitle} value={newTitle} />
+        <Textinput onChange={setnewDesription} value={newDescription} />
+        <Button
+          onPress={handleEdit}
+          title="Submit Editing"
+          style={styles.button}
+          loading={loading}
+        />
       </Container>
     </>
   );
