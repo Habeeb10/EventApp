a3d7ac32e2e223d0f5894af448a7bcd07bd632ac
Tijani Habeeb <79145763+habeeb10@users.noreply.github.com>
2021-08-20 01:06:07 +0100

    feat: installed app packages

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Provider } from "react-redux";
 import Navigator from "./src/navigator/navigation";
 import { Event } from "./src/screens/event/Event";
+import configureStore from "./src/store/configureStore";
+
+const store = configureStore();
 
 export default function App() {
-  return <Navigator />;
+  return (
+    <Provider store={store}>
+      <Navigator />
+    </Provider>
+  );
 }
